a48f506b8e9e65f8eb0a62e915dda2d6bf2e1fce
gaoshiyao <gaoshiyao@bd-apaas.com>
2019-04-12 20:51:26 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
+    <headers/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import header from "@/components/header";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  // props: {
+  //   source: String
+  // },
+  components: {
+    headers: header
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/detail.vue
+++ src/components/detail.vue
@@ -12,7 +12,10 @@
         <el-table-column prop="name" label="姓名" width="120"></el-table-column>
         <el-table-column prop="address" label="地址" show-overflow-tooltip>
           <template slot-scope="scope">
-            <el-button type="text" size="small" @click="detail()">{{ scope.row.address }}</el-button>
+            <el-button
+              type="text"
+              size="small"
+            >{{ scope.row.address }}</el-button>
           </template>
         </el-table-column>
         <el-table-column label="日期" width="120">
@@ -33,37 +36,37 @@ export default {
       tableData: [
         {
           date: "2016-05-03",
-          name: "王小虎",
+          name: "高士尧",
           address: "上海市普陀区金沙江路 1518 弄"
         },
         {
           date: "2016-05-02",
-          name: "王小虎",
+          name: "高士尧",
           address: "上海市普陀区金沙江路 1518 弄"
         },
         {
           date: "2016-05-04",
-          name: "王小虎",
+          name: "高士尧",
           address: "上海市普陀区金沙江路 1518 弄"
         },
         {
           date: "2016-05-01",
-          name: "王小虎",
+          name: "高士尧",
           address: "上海市普陀区金沙江路 1518 弄"
         },
         {
           date: "2016-05-08",
-          name: "王小虎",
+          name: "高士尧",
           address: "上海市普陀区金沙江路 1518 弄"
         },
         {
           date: "2016-05-06",
-          name: "王小虎",
+          name: "高士尧",
           address: "上海市普陀区金沙江路 1518 弄"
         },
         {
           date: "2016-05-07",
-          name: "王小虎",
+          name: "高士尧",
           address: "上海市普陀区金沙江路 1518 弄"
         }
       ],
@@ -71,9 +74,6 @@ export default {
     };
   },
   methods: {
-    detail(){
-      this.$router.push('/test')
-    },
     toggleSelection(rows) {
       if (rows) {
         rows.forEach(row => {
